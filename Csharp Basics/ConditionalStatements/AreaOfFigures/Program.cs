004afd83dbebd2e4f386fc1ed5bbf3c11f890353
Aleksandar Nikolov <145496882+AleksandarNiko@users.noreply.github.com>
2024-11-30 20:52:58 +0200

    Add files via upload

---
+++ Csharp Basics/ConditionalStatements/AreaOfFigures/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace AreaOfFigures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "George";
+            name[2] = "m";
+            Console.WriteLine(name[2]);
+            string type = Console.ReadLine();
+            if (type == "square")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double area = a * a;
+                Console.WriteLine($"{area:F3}");
+            }
+            else if (type =="rectangle")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                double sideB =double.Parse(Console.ReadLine());
+                double areas = sideA*sideB;
+                Console.WriteLine($"{areas:F3}");
+            }
+            else if(type =="circle")
+            {
+                double radius = double.Parse(Console.ReadLine());
+                double Area = Math.PI * Math.Pow(radius,2);
+                Console.WriteLine($"{Area:F3}");
+            }
+            else if (type =="triangle")
+            {
+                double side1 = double.Parse(Console.ReadLine());
+                double high = double.Parse(Console.ReadLine());
+                double areA = (side1 * high) / 2;
+                Console.WriteLine($"{areA:F3}");
+            }
+        }
+
+    }
+}
---
+++ Csharp Basics/ConditionalStatements/GuessPassword/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GuessPassword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+            string input = "s3cr3t!P@ssw0rd";
+            if (password == input)
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
